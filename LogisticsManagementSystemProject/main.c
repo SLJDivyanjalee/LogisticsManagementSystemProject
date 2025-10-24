39298c20645d5234e8f703e6e67d662a2edff8e8
SLJDivyanjalee <as20240582@sci.sjp.ac.lk>
2025-10-24 17:58:37 +0530

    add city management with add,rename,search,view,delete

--- LogisticsManagementSystemProject/main.c
+++ LogisticsManagementSystemProject/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-mainMenu();
+#define MAX_CITIES 30
+#define NAME_LENGTH 50
+
+void mainMenu();
+void cityManagement();
+
+//city management functions
+void addCity();
+void displayCities();
+void renameCity();
+void removeCity();
+int searchCity(char name[]);
+void toLowerCase(char str[]);
+
+char cities[MAX_CITIES][NAME_LENGTH];
+int cityCount = 0;
 
 int main()
 {
@@ -33,6 +49,7 @@ void mainMenu(){
         switch(choice){
             case 1:
                 printf("\n--- City Management ---\n");
+                cityManagement();
                 break;
             case 2:
                 printf("\n--- Distance Management ---\n");
@@ -67,3 +84,155 @@ void mainMenu(){
         }
     } while(choice != 10);
 }
+
+// -------------------------- City Management ----------------------
+void cityManagement() {
+    int choice;
+    do {
+        printf(" \tMenu \n");
+        printf("1. Add a new city\n");
+        printf("2. Display all cities\n");
+        printf("3. Rename a city\n");
+        printf("4. Remove a city\n");
+        printf("5. Back to Main Menu\n");
+        printf("Enter your choice (1-5): ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                addCity(); //add a new city
+                break;
+            case 2:
+                displayCities(); //view all cities
+                break;
+            case 3:
+                renameCity(); // rename a city
+                break;
+            case 4:
+                removeCity(); //delete a city
+                break;
+            case 5:
+                printf("Returning to Main Menu...\n");
+                break;
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
+    } while (choice != 5);
+}
+
+void addCity() {
+    if (cityCount >= MAX_CITIES) {
+        printf("Can't add more cities; list is full.\n");
+        return;
+    }
+
+    char cityName[NAME_LENGTH];
+    printf("Enter city name: ");
+    fgets(cityName, NAME_LENGTH, stdin);
+    cityName[strcspn(cityName, "\n")] = '\0'; // remove newline
+
+    if (strlen(cityName) == 0) {
+        printf("City name can't be empty!\n");
+        return;
+    }
+
+    if (searchCity(cityName) != -1) {
+        printf("City '%s' already exists!\n", cityName);
+        return;
+    }
+
+    strcpy(cities[cityCount], cityName);
+    cityCount++;
+    printf("%s added successfully!\n", cityName);
+}
+
+void displayCities() {
+    if (cityCount == 0) {
+        printf("No cities in the system.\n");
+        return;
+    }
+
+    printf("=== List of Cities (%d/%d) ===\n", cityCount, MAX_CITIES);
+    for (int i = 0; i < cityCount; i++) {
+        printf("%d. %s\n", i + 1, cities[i]);
+    }
+}
+
+int searchCity(char name[]) {
+    char searchCity[NAME_LENGTH];
+    strcpy(searchCity, name);
+    toLowerCase(searchCity);
+
+    for (int i = 0; i < cityCount; i++) {
+        char currentCity[NAME_LENGTH];
+        strcpy(currentCity, cities[i]);
+        toLowerCase(currentCity);
+
+        if (strcmp(currentCity, searchCity) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void toLowerCase(char str[]) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] = str[i] + 32;
+        }
+    }
+}
+
+void renameCity() {
+    if (cityCount == 0) {
+        printf("No cities available\n");
+        return;
+    }
+
+    char cityName[NAME_LENGTH], newCityName[NAME_LENGTH];
+    printf("Enter city name you want to rename :");
+    scanf("%[^\n]", cityName); // read entire line including spaces
+    getchar(); // clear the newline from buffer
+
+    int index = searchCity(cityName);
+    if (index == -1) {
+        printf("%s not found!\n", cityName);
+        return;
+    }
+
+    printf("Enter new city name: ");
+    scanf("%c",cityName);
+
+    if (strlen(newCityName) == 0) {
+        printf("city name can't be empty!\n");
+        return;
+    }
+
+    printf("City renamed from '%s' to '%s'\n", cities[index], newCityName);
+    strcpy(cities[index], newCityName); //overwrite city name
+}
+
+void removeCity() {
+    if (cityCount == 0) {
+        printf("No cities available\n");
+        return;
+    }
+
+    char cityName[NAME_LENGTH];
+    printf("Enter city name you want to remove:");
+    scanf("%[^\n]", cityName); // read entire line including spaces
+    getchar(); // clear the newline from buffer
+
+    int index = searchCity(cityName);
+    if (index == -1) {
+        printf("Error: City '%s' not found!\n", cityName);
+        return;
+    }
+
+    for (int i = index; i < cityCount - 1; i++) {
+        strcpy(cities[i], cities[i + 1]);  //copies a string from one array position to another
+    }
+    cityCount--;
+    printf("City '%s' removed successfully!\n", cityName);
+    }
+
